87bac2482eff4bea852b85d04bac4f0684943748
az2 <anggacipiet89@gmail.com>
2024-07-19 00:51:11 +0700

    Update Features.tsx

--- src/templates/Features.tsx
+++ src/templates/Features.tsx
@@ -59,6 +59,12 @@ const Features = () => {
           ))}
         </div>
       </div>
+      <div className="mt-20 text-center">
+        <Link href="/products"
+          className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200">
+          View All Products
+        </Link>
+      </div>
     </div>
   );
 }
