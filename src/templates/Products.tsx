f3c6269259274ed7e36509553da4568bf23cd52f
az2 <anggacipiet89@gmail.com>
2024-07-19 02:21:44 +0700

    Update Products.tsx

--- src/templates/Products.tsx
+++ src/templates/Products.tsx
@@ -48,14 +48,14 @@ const Products = () => {
                 <CardItem
                   translateZ={20}
                   as="button"
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  className="px-4 py-2 rounded-xl text-xs font-normal text-white"
                 >
                   Spesifikasi →
                 </CardItem>
                 <CardItem
                   translateZ={20}
                   as="button"
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                  className="px-4 py-2 rounded-xl bg-black bg-white dark:text-black text-white text-xs font-bold"
                 >
                   Konsultasi
                 </CardItem>
