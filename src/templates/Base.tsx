a5a2604a26cad7c42df54da66114edbb01f3b1bc
az2 <anggacipiet89@gmail.com>
2024-07-20 01:42:59 +0700

    Update Base.tsx

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -2,7 +2,7 @@ import { Meta } from '@/component/layout/Meta';
 import { AppConfig } from '@/utils/AppConfig';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
-import { Hero } from './Hero';
+//import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
 import { Features } from './Features';
 import { FAQ } from './Faq';
