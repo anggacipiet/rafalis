f47e3299216e51415277e7c2b662a22685471fc5
az2 <anggacipiet89@gmail.com>
2024-07-29 10:10:57 +0700

    fix google chrome

--- src/component/video/ModalVideo.tsx
+++ src/component/video/ModalVideo.tsx
@@ -34,6 +34,17 @@ export default function ModalVideo({
     }
   }
 
+  const handleModalOpen = () => {
+    setModalOpen(true)
+    setTimeout(() => {
+      if (videoRef.current) {
+        videoRef.current.play().catch(error => {
+          console.error("Failed to play video:", error)
+        })
+      }
+    }, 100) // Adjust the delay as needed
+  }
+
   return (
     <div>
       {/* Video Thumbnail */}
@@ -42,7 +53,7 @@ export default function ModalVideo({
           <div className="flex flex-col justify-center">
             <Image src={thumb} width={thumbWidth} height={thumbHeight} alt={thumbAlt} />
           </div>
-          <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={() => setModalOpen(true)}>
+          <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={handleModalOpen}>
             <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
               <path d="M10 17l6-5-6-5z" />
@@ -52,7 +63,12 @@ export default function ModalVideo({
         </div>
       </div>
 
-      <Transition show={modalOpen} as={Fragment} afterEnter={() => videoRef.current?.play()}>
+      <Transition show={modalOpen} as={Fragment} afterEnter={() => { if (videoRef.current) {
+                videoRef.current.play().catch(error => {
+                  console.error("Failed to play video:", error)
+                })
+              }
+        }}>
         <Dialog initialFocus={videoRef} onClose={handleModalClose}>
           {/* Modal Backdrop */}
           <Transition.Child
@@ -80,7 +96,7 @@ export default function ModalVideo({
           >
             <div className="max-w-6xl mx-auto h-full flex items-center">
               <Dialog.Panel className="w-full max-h-full aspect-video bg-black overflow-hidden">
-                <video ref={videoRef} width={videoWidth} height={videoHeight} controls>
+                <video ref={videoRef} width={videoWidth} height={videoHeight} loop controls>
                   <source src={video} type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
