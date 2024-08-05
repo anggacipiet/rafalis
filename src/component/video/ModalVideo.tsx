321df24136c420dab85244eaa51644b24d965760
az2 <anggacipiet89@gmail.com>
2024-08-05 11:57:03 +0700

    Update ModalVideo.tsx

--- src/component/video/ModalVideo.tsx
+++ src/component/video/ModalVideo.tsx
@@ -5,7 +5,7 @@ import { Dialog, Transition } from '@headlessui/react'
 import Image from 'next/image'
 
 interface ModalVideoProps {
-  thumb: StaticImageData | string
+  thumb: StaticImageData
   thumbWidth: number
   thumbHeight: number
   thumbAlt: string
@@ -26,18 +26,6 @@ export default function ModalVideo({
   const [modalOpen, setModalOpen] = useState<boolean>(false)
   const videoRef = useRef<HTMLVideoElement>(null)
 
-  const handleModalClose = () => {
-    setModalOpen(false)
-    if (videoRef.current) {
-      videoRef.current.pause()
-      videoRef.current.currentTime = 0
-    }
-  }
-
-  const handleModalOpen = () => {
-    setModalOpen(true)
-  }
-
   return (
     <div>
       {/* Video Thumbnail */}
@@ -46,10 +34,7 @@ export default function ModalVideo({
           <div className="flex flex-col justify-center">
             <Image src={thumb} width={thumbWidth} height={thumbHeight} alt={thumbAlt} />
           </div>
-          <button
-            className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg"
-            onClick={handleModalOpen}
-          >
+          <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={() => { setModalOpen(true) }}>
             <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
               <path d="M10 17l6-5-6-5z" />
@@ -59,12 +44,11 @@ export default function ModalVideo({
         </div>
       </div>
 
-      <Transition show={modalOpen} as={Fragment}>
-        <Dialog initialFocus={videoRef} onClose={handleModalClose}>
+      <Transition show={modalOpen} as={Fragment} afterEnter={() => videoRef.current?.play()}>
+        <Dialog initialFocus={videoRef} onClose={() => setModalOpen(false)}>
           {/* Modal Backdrop */}
           <Transition.Child
-            as="div"
-            className="fixed inset-0 z-50 bg-black bg-opacity-75 transition-opacity"
+            className="fixed inset-0 z-[99999] bg-black bg-opacity-75 transition-opacity"
             enter="transition ease-out duration-200"
             enterFrom="opacity-0"
             enterTo="opacity-100"
@@ -76,25 +60,17 @@ export default function ModalVideo({
 
           {/* Modal Dialog */}
           <Transition.Child
-            as="div"
-            className="fixed inset-0 z-50 overflow-hidden flex items-center justify-center transform px-4 sm:px-6"
+            className="fixed inset-0 z-[99999] overflow-hidden flex items-center justify-center transform px-4 sm:px-6"
             enter="transition ease-out duration-200"
             enterFrom="opacity-0 scale-95"
             enterTo="opacity-100 scale-100"
-            leave="transition ease-out duration-200"
-            leaveFrom="opacity-100 scale-100"
+            leave="ttransition ease-out duration-200"
+            leaveFrom="oopacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
             <div className="max-w-6xl mx-auto h-full flex items-center">
               <Dialog.Panel className="w-full max-h-full aspect-video bg-black overflow-hidden">
-                <video
-                  ref={videoRef}
-                  width={videoWidth}
-                  height={videoHeight}
-                  loop
-                  controls
-                  preload="auto"
-                >
+                <video ref={videoRef} width={videoWidth} height={videoHeight} loop controls>
                   <source src={video} type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
